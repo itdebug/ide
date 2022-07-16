3aef09acb926cfa553863168b6f3f526085b9f08
rklu <rklu@ctrip.com>
2022-07-16 23:32:18 +0800

    ide登录

---
+++ ide-local/src/main/java/com/example/idelocal/IdeLocalApplication.java
@@ -0,0 +1,13 @@
+package com.example.idelocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeLocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeLocalApplication.class, args);
+    }
+
+}
--- readme.md
+++ readme.md
@@ -1,8 +1,9 @@
 # 步骤
 
 ## 第一步
-ide第一次安装，需要将file文件夹下的JetbrainsCrack-2.7-release-str放到
-bin目录下
+ide第一次安装，需要将file文件夹下的
+JetbrainsCrack-2.7-release-str.jar
+放到bin目录下
 
 ## 第二步
 第一次登录需要临时验证码，可以到http://idea.hicxy.com/该网址下去获取
